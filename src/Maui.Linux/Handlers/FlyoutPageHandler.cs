a7bf4d1bdce6a3b1be06e7e3ca0c5da7026c2261
redth <jondick@gmail.com>
2026-02-12 17:59:34 -0500

    Add sidebar collapse/expand toggle button to FlyoutPage
    - Wrap Paned in Gtk.Box with a toggle bar containing ☰ hamburger button
    - Clicking toggles sidebar visibility and syncs FlyoutPage.IsPresented
    - Remembers last Paned position and restores on expand
    - MapIsPresented also handles collapse/expand programmatically

--- src/Maui.Linux/Handlers/FlyoutPageHandler.cs
+++ src/Maui.Linux/Handlers/FlyoutPageHandler.cs
@@ -3,8 +3,12 @@ using Microsoft.Maui.Platform;
 
 namespace Maui.Linux.Handlers;
 
-public class FlyoutPageHandler : GtkViewHandler<IFlyoutView, Gtk.Paned>
+public class FlyoutPageHandler : GtkViewHandler<IFlyoutView, Gtk.Box>
 {
+	Gtk.Paned? _paned;
+	Gtk.Button? _toggleButton;
+	int _lastPosition = 250;
+
 	public static new IPropertyMapper<IFlyoutView, FlyoutPageHandler> Mapper =
 		new PropertyMapper<IFlyoutView, FlyoutPageHandler>(ViewMapper)
 		{
@@ -17,27 +21,81 @@ public class FlyoutPageHandler : GtkViewHandler<IFlyoutView, Gtk.Paned>
 	{
 	}
 
-	protected override Gtk.Paned CreatePlatformView()
+	protected override Gtk.Box CreatePlatformView()
 	{
-		var paned = Gtk.Paned.New(Gtk.Orientation.Horizontal);
-		paned.SetPosition(250); // default flyout width
-		paned.SetVexpand(true);
-		paned.SetHexpand(true);
-		paned.SetResizeStartChild(false);
-		paned.SetShrinkStartChild(false);
-		paned.SetResizeEndChild(true);
-		paned.SetShrinkEndChild(false);
-		return paned;
+		var container = Gtk.Box.New(Gtk.Orientation.Vertical, 0);
+		container.SetVexpand(true);
+		container.SetHexpand(true);
+
+		// Toggle bar with hamburger button
+		var toggleBar = Gtk.Box.New(Gtk.Orientation.Horizontal, 0);
+		_toggleButton = Gtk.Button.New();
+		_toggleButton.SetLabel("☰");
+		_toggleButton.AddCssClass("flat");
+		_toggleButton.SetTooltipText("Toggle sidebar");
+		_toggleButton.OnClicked += OnToggleClicked;
+		toggleBar.Append(_toggleButton);
+
+		var sep = Gtk.Separator.New(Gtk.Orientation.Horizontal);
+
+		container.Append(toggleBar);
+		container.Append(sep);
+
+		// Paned for flyout + detail
+		_paned = Gtk.Paned.New(Gtk.Orientation.Horizontal);
+		_paned.SetPosition(250);
+		_paned.SetVexpand(true);
+		_paned.SetHexpand(true);
+		_paned.SetResizeStartChild(false);
+		_paned.SetShrinkStartChild(false);
+		_paned.SetResizeEndChild(true);
+		_paned.SetShrinkEndChild(false);
+		container.Append(_paned);
+
+		return container;
+	}
+
+	void OnToggleClicked(Gtk.Button sender, EventArgs args)
+	{
+		if (_paned == null) return;
+
+		var startChild = _paned.GetStartChild();
+		if (startChild == null) return;
+
+		if (startChild.GetVisible())
+		{
+			// Collapse: save position, hide sidebar
+			_lastPosition = _paned.GetPosition();
+			startChild.SetVisible(false);
+			_paned.SetPosition(0);
+		}
+		else
+		{
+			// Expand: restore sidebar
+			startChild.SetVisible(true);
+			_paned.SetPosition(_lastPosition);
+		}
+
+		// Sync MAUI's IsPresented
+		if (VirtualView is Microsoft.Maui.Controls.FlyoutPage fp)
+			fp.IsPresented = startChild.GetVisible();
+	}
+
+	protected override void DisconnectHandler(Gtk.Box platformView)
+	{
+		if (_toggleButton != null)
+			_toggleButton.OnClicked -= OnToggleClicked;
+		base.DisconnectHandler(platformView);
 	}
 
 	public static void MapFlyout(FlyoutPageHandler handler, IFlyoutView flyoutView)
 	{
 		_ = handler.MauiContext ?? throw new InvalidOperationException("MauiContext not set.");
 
-		if (flyoutView.Flyout != null)
+		if (flyoutView.Flyout != null && handler._paned != null)
 		{
 			var platformFlyout = (Gtk.Widget)flyoutView.Flyout.ToPlatform(handler.MauiContext);
-			handler.PlatformView?.SetStartChild(platformFlyout);
+			handler._paned.SetStartChild(platformFlyout);
 		}
 	}
 
@@ -45,16 +103,30 @@ public class FlyoutPageHandler : GtkViewHandler<IFlyoutView, Gtk.Paned>
 	{
 		_ = handler.MauiContext ?? throw new InvalidOperationException("MauiContext not set.");
 
-		if (flyoutView.Detail != null)
+		if (flyoutView.Detail != null && handler._paned != null)
 		{
 			var platformDetail = (Gtk.Widget)flyoutView.Detail.ToPlatform(handler.MauiContext);
-			handler.PlatformView?.SetEndChild(platformDetail);
+			handler._paned.SetEndChild(platformDetail);
 		}
 	}
 
 	public static void MapIsPresented(FlyoutPageHandler handler, IFlyoutView flyoutView)
 	{
-		var startChild = handler.PlatformView?.GetStartChild();
-		startChild?.SetVisible(flyoutView.IsPresented);
+		if (handler._paned == null) return;
+
+		var startChild = handler._paned.GetStartChild();
+		if (startChild == null) return;
+
+		if (flyoutView.IsPresented)
+		{
+			startChild.SetVisible(true);
+			handler._paned.SetPosition(handler._lastPosition);
+		}
+		else
+		{
+			handler._lastPosition = handler._paned.GetPosition();
+			startChild.SetVisible(false);
+			handler._paned.SetPosition(0);
+		}
 	}
 }

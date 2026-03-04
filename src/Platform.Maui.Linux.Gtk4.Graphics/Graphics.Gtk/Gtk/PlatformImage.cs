a50e1c68b6276493b91504d2953f37034d93b562
lytico <lytico@users.sourceforge.net>
2026-03-04 23:33:34 +0100

    Refactor `PlatformImage` for consistent formatting and improved stream handling in Graphics.Gtk.

--- src/Platform.Maui.Linux.Gtk4.Graphics/Graphics.Gtk/Gtk/PlatformImage.cs
+++ src/Platform.Maui.Linux.Gtk4.Graphics/Graphics.Gtk/Gtk/PlatformImage.cs
@@ -2,78 +2,76 @@ using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Gio;
+using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.Maui.Graphics.Platform.Gtk;
 
-public class PlatformImage : IImage
-{
-	public PlatformImage(GdkPixbuf.Pixbuf pix)
-	{
-		_pixbuf = pix;
-	}
+public class PlatformImage : IImage {
+    public PlatformImage (GdkPixbuf.Pixbuf pix) {
+        _pixbuf = pix;
+    }
 
-	private GdkPixbuf.Pixbuf? _pixbuf;
+    private GdkPixbuf.Pixbuf? _pixbuf;
 
-	// https://developer.gnome.org/gdk-pixbuf/stable/gdk-pixbuf-The-GdkPixbuf-Structure.html
-	public GdkPixbuf.Pixbuf? NativeImage => _pixbuf;
+    // https://developer.gnome.org/gdk-pixbuf/stable/gdk-pixbuf-The-GdkPixbuf-Structure.html
+    public GdkPixbuf.Pixbuf? NativeImage => _pixbuf;
 
-	public void Draw(ICanvas canvas, RectF dirtyRect)
-	{
-		canvas.DrawImage(this, dirtyRect.Left, dirtyRect.Top, (float)Math.Round(dirtyRect.Width), (float)Math.Round(dirtyRect.Height));
-	}
+    public void Draw (ICanvas canvas, RectF dirtyRect) {
+        canvas.DrawImage (this, dirtyRect.Left, dirtyRect.Top, (float)Math.Round (dirtyRect.Width), (float)Math.Round (dirtyRect.Height));
+    }
 
-	public void Dispose()
-	{
-		var previousValue = Interlocked.Exchange(ref _pixbuf, null);
-		previousValue?.Dispose();
-	}
+    public void Dispose () {
+        var previousValue = Interlocked.Exchange (ref _pixbuf, null);
+        previousValue?.Dispose ();
+    }
 
-	public float Width => NativeImage?.Width ?? 0;
+    public float Width => NativeImage?.Width ?? 0;
 
-	public float Height => NativeImage?.Width ?? 0;
+    public float Height => NativeImage?.Width ?? 0;
 
-	[GtkMissingImplementation]
-	public IImage Downsize(float maxWidthOrHeight, bool disposeOriginal = false)
-	{
-		return this;
-	}
+    [GtkMissingImplementation]
+    public IImage Downsize (float maxWidthOrHeight, bool disposeOriginal = false) {
+        return this;
+    }
 
-	[GtkMissingImplementation]
-	public IImage Downsize(float maxWidth, float maxHeight, bool disposeOriginal = false)
-	{
-		return this;
-	}
+    [GtkMissingImplementation]
+    public IImage Downsize (float maxWidth, float maxHeight, bool disposeOriginal = false) {
+        return this;
+    }
 
-	[GtkMissingImplementation]
-	public IImage Resize(float width, float height, ResizeMode resizeMode = ResizeMode.Fit, bool disposeOriginal = false)
-	{
-		return this;
-	}
+    [GtkMissingImplementation]
+    public IImage Resize (float width, float height, ResizeMode resizeMode = ResizeMode.Fit, bool disposeOriginal = false) {
+        return this;
+    }
 
-	public void Save(Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1)
-	{
-		NativeImage.SaveToStream(stream, format, quality);
-	}
+    public void Save (Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1) {
+        NativeImage.SaveToStream (stream, format, quality);
+    }
 
-	public async Task SaveAsync(Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1)
-	{
-		await Task.Run(() => NativeImage.SaveToStream(stream, format, quality));
-	}
+    public async Task SaveAsync (Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1) {
+        await Task.Run (() => NativeImage.SaveToStream (stream, format, quality));
+    }
 
-	public IImage ToImage(int width, int height, float scale = 1f)
-	{
-		using var context = new PlatformBitmapExportContext(width, height, scale);
-		context.Canvas.Scale(scale, scale);
-		Draw(context.Canvas, new RectF(0, 0, (float)width / scale, (float)height / scale));
-		return context.Image;
-	}
+    public IImage ToImage (int width, int height, float scale = 1f) {
+        using var context = new PlatformBitmapExportContext (width, height, scale);
+        context.Canvas.Scale (scale, scale);
+        Draw (context.Canvas, new RectF (0, 0, (float)width / scale, (float)height / scale));
+        return context.Image;
+    }
 
-	public IImage ToPlatformImage() => this;
+    public IImage ToPlatformImage () => this;
 
-	public static IImage FromStream(Stream stream, ImageFormat formatHint)
-	{
-		var platformHint = formatHint.ToImageExtension();
-		var pix = new GdkPixbuf.Pixbuf(stream);
-		return new PlatformImage(pix);
-	}
+    public static IImage FromStream (Stream stream, ImageFormat formatHint) {
+        // Read stream into a byte array
+        byte[] buffer = new byte[stream.Length];
+        stream.ReadExactly (buffer); // Reads entire stream into buffer
+        using var glibBytes = GLib.Bytes.New (buffer);
+
+        using var gioStream = new MemoryInputStream ();
+        gioStream.AddBytes (glibBytes);
+        var pix = GdkPixbuf.Pixbuf.NewFromStream (gioStream, null);
+
+        return new PlatformImage (pix);
+    }
 }
